c98a8b56cdfe185ef211323aa421f147624fbbd7
simkass <simkassab@gmail.com>
2022-01-30 15:57:52 -0500

    Added different subtypes to config file

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 URL_base = "https://duproprio.com/en/search/list?search=true&type%5B0%5D=house&subtype%5B0%5D=1&subtype%5B1%5D=2&subtype%5B2%5D=4&subtype%5B3%5D=5&subtype%5B4%5D=6&subtype%5B5%5D=7&subtype%5B6%5D=9&subtype%5B7%5D=10&subtype%5B8%5D=11&subtype%5B9%5D=13&subtype%5B10%5D=15&subtype%5B11%5D=17&subtype%5B12%5D=19&subtype%5B13%5D=21&subtype%5B14%5D=97&subtype%5B15%5D=99&subtype%5B16%5D=100&is_sold=1&with_builders=1&parent=1&pageNumber="
+
+CONDO_URL_base = "https://duproprio.com/en/search/list?search=true&subtype%5B0%5D=14&subtype%5B1%5D=12&subtype%5B2%5D=3&is_sold=1&with_builders=1&parent=1&pageNumber="
+
 URL_end = "&sort=-published_at"
 
 home_subtypes = [['bungalow', 'semi-detached', 'hab-2-storey',
@@ -11,3 +14,5 @@ home_subtypes = [['bungalow', 'semi-detached', 'hab-2-storey',
                  'Mobile home', 'Acreage / Hobby Farm / Ranch', 'Villa', 
                  'Raised Bungalow', '3 Storey', 'Manufactured home', '4 Storey', 
                  'Misc.', 'Bi-Level', 'Bi-generation']]
+
+condo_subtypes = [['hab-condominium', 'hab-loft', 'hab-penthouse'], ['Condominium', 'Loft', 'Penthouse']]
