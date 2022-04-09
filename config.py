f84c86556e056a5fb524d47fcf9371db46f8f18f
Simon Kassab <simkassab@gmail.com>
2022-04-08 20:36:54 -0400

    Clean and format urls scraper.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-URL_base = "https://duproprio.com/en/search/list?search=true&type%5B0%5D=house&subtype%5B0%5D=1&subtype%5B1%5D=2&subtype%5B2%5D=4&subtype%5B3%5D=5&subtype%5B4%5D=6&subtype%5B5%5D=7&subtype%5B6%5D=9&subtype%5B7%5D=10&subtype%5B8%5D=11&subtype%5B9%5D=13&subtype%5B10%5D=15&subtype%5B11%5D=17&subtype%5B12%5D=19&subtype%5B13%5D=21&subtype%5B14%5D=97&subtype%5B15%5D=99&subtype%5B16%5D=100&is_sold=1&with_builders=1&parent=1&pageNumber="
+# URL values
+home_url_base = 'https://duproprio.com/en/search/list?search=true&type%5B0%5D=house&subtype%5B0%5D=1&subtype%5B1%5D=2&subtype%5B2%5D=4&subtype%5B3%5D=5&subtype%5B4%5D=6&subtype%5B5%5D=7&subtype%5B6%5D=9&subtype%5B7%5D=10&subtype%5B8%5D=11&subtype%5B9%5D=13&subtype%5B10%5D=15&subtype%5B11%5D=17&subtype%5B12%5D=19&subtype%5B13%5D=21&subtype%5B14%5D=97&subtype%5B15%5D=99&subtype%5B16%5D=100&is_sold=1&with_builders=1&parent=1&pageNumber='
+condo_url_base = 'https://duproprio.com/en/search/list?search=true&subtype%5B0%5D=14&subtype%5B1%5D=12&subtype%5B2%5D=3&is_sold=1&with_builders=1&parent=1&pageNumber='
+url_end = '&sort=-published_at'
 
-CONDO_URL_base = "https://duproprio.com/en/search/list?search=true&subtype%5B0%5D=14&subtype%5B1%5D=12&subtype%5B2%5D=3&is_sold=1&with_builders=1&parent=1&pageNumber="
 
-URL_end = "&sort=-published_at"
 
+######
 home_subtypes = [['bungalow', 'semi-detached', 'hab-2-storey',
                  '1-12-storey', 'split-level', 'townhouse', 
                  'country-home', 'mobile-home', 'acreage-hobby-farm-ranch', 
---
+++ scripts/duproprio_urls_scraper.py
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------
+# duproprio_urls_scraper.py
+# Web scraping script to acquire listing urls from duproprio.com
+# ------------------------------------------------------------------
+
+import requests
+from config import url_end
+from bs4 import BeautifulSoup
+
+
+def scrape_listings_page(filename: str, page: requests.Response) -> int:
+    """Parse all house listings from a single duproprio listings preview page.
+
+    Args:
+        filename (str): .txt file where listing urls are stored line by line.
+        page (requests.Response): Listings preview page.
+
+    Returns:
+        int: Number of listings parsed from the page.
+    """
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    links = 0
+    for a in soup.find_all('a', href=True, class_='search-results-listings-list__item-image-link'):
+        with open(filename, 'a') as f:
+            f.write(str(a['href']) + '\n')
+        links += 1
+
+    return links
+
+
+def scrape_listing_urls(filename: str, url_base: str, start_page: int, end_page: int):
+    """Parse all house listings from duproprio listings preview pages between specified start and end.
+
+    Args:
+        filename (str): .txt file where listing urls are stored line by line.
+        url_base (str): The base url (before the page index) for the listing preview pages.
+        start_page (int): Start page index of the listing preview pages.
+        end_page (int): End page index of the listing preview pages.
+    """
+    for i in range(start_page, end_page):
+        page = requests.get(url_base + str(i) + url_end)
+
+        if page.status_code == 200:
+            links = scrape_listings_page(filename, page)
+            print('Parsed ' + str(links) + ' listing URLs from page ' + str(i))
