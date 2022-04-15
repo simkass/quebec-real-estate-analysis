733ccad96f39cf2e3fdae442d1977e79f0b1f896
Simon Kassab <simkassab@gmail.com>
2022-04-15 18:58:42 -0400

    Fix filenames

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import config
 
 if __name__ == '__main__':
     # Scrape Family Home Listings (269 pages on Duproprio)
-    scrape_listing_urls('../data/raw1/home_listings_urls.csv', config.home_url_base, 1, 269)
-    scrape_listings(config.home_subtypes, '../data/raw1/home_listings_urls.csv', '../data/raw1/home_raw_listings.csv')
+    scrape_listing_urls('../data/raw/home_listings_urls.txt', config.home_url_base, 1, 269)
+    scrape_listings(config.home_subtypes, '../data/raw/home_listings_urls.txt', '../data/raw1/home_raw_listings.csv')
 
     # Scrape Condo Listings (122 pages on Duproprio)
-    scrape_listing_urls('../data/raw1/condo_listings_urls.csv', config.home_url_base, 1, 122)
-    scrape_listings(config.home_subtypes, '../data/raw1/condo_listings_urls.csv', '../data/raw1/condo_raw_listings.csv')
+    scrape_listing_urls('../data/raw/condo_listings_urls.txt', config.home_url_base, 1, 122)
+    scrape_listings(config.home_subtypes, '../data/raw/condo_listings_urls.txt', '../data/raw1/condo_raw_listings.csv')
